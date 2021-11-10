5407221f0ab41f808d244f5e668bf08584b4d24a
Alex Catahan <rac4sq@virginia.edu>
2021-11-10 16:36:59 +0000

    hw 7 progress

--- src/homework/hw7.lean
+++ src/homework/hw7.lean
@@ -1,4 +1,5 @@
 import data.set
+import tactic.ring
 
 namespace relation
 
@@ -36,6 +37,15 @@ your answer (in English).
 -/
 example : (∃ (b : β), true) → asymmetric r → ¬reflexive r :=
 begin
+  assume h,
+  cases h with w pf,
+  unfold asymmetric,
+  unfold reflexive,
+  assume h,
+  assume j,
+  have a := j w,
+  have b := h a,
+  contradiction,
 end
 
 
@@ -57,8 +67,17 @@ stuck, then you need to figure out an additional condition that needs
 to be added as a premise to make the proposition true. In that case,
 add the premise and then show that the updated conjecture is true.
 -/
-example : transitive r → reflexive r → ¬ asymmetric r :=
+example : (∃ (b : β), true) → transitive r → reflexive r → ¬ asymmetric r :=
 begin
+   unfold transitive reflexive asymmetric,
+  assume h,
+  assume trans,
+  assume refl,
+  assume asymm,
+  cases h with w pf,
+  have r_w_w := refl w,
+  have notr_w_w := asymm r_w_w,
+  contradiction,
 end
 
 
@@ -76,6 +95,19 @@ example : ∀ (s : set β)
             s2 ⊆ s1 → 
             s1 = s2 :=
 begin
+  assume s s1 s2,
+  assume s1ins s2ins,
+  assume s1s2 s2s1,
+  apply set.ext,
+  assume x,
+  split,
+  assume xs1,
+  have xins2 := s1s2 xs1,
+  apply xins2,
+  assume xins2,
+  have xins1 := s2s1 xins2,
+  assumption,
+
 end
 
 
@@ -99,21 +131,51 @@ into English. (Or if you wanted to be truly Hobbit-like you could say
 -- A: For any n, 1 divides n.
 example : ∀ n, divides 1 n :=
 begin
+  assume n, 
+  unfold divides,
+  cases n,
+  apply exists.intro 0,
+  refl,
+  apply exists.intro n.succ,
+  ring,
+  
+  
+  
 end
 
 -- B. For any n, n divides n
 example : ∀ n, divides n n :=
 begin
+  assume n,
+  unfold divides,
+  apply exists.intro 1,
+  ring,
 end
 
 -- #C. prove that divides is reflexive 
 example : reflexive divides :=
 begin
+  unfold reflexive divides,
+  assume x,
+  apply exists.intro 1,
+  ring,
+  
 end 
 
 -- #D. prove that divides is transitive
 example : transitive divides :=
 begin
+  unfold transitive divides,
+  assume x y z,
+  assume xy,
+  assume yz,
+  cases xy with k1 xdy,
+  cases yz with k2 ydz,
+  apply exists.intro(k2*k1),
+  rw ydz,
+  rw xdy,
+  ring,
+
 end 
 
 /- 
@@ -129,6 +191,17 @@ it's not.
 -/
 example : anti_symmetric divides := 
 begin  
+  unfold anti_symmetric divides,
+  assume x y xy yx,
+  cases xy with w pf,
+  cases yx with w2 pf2,
+  
+  
+
+
+
+
+
 end
 
 
@@ -144,11 +217,23 @@ problems.
 -- A
 example : asymmetric r → irreflexive r :=
 begin
+  unfold asymmetric irreflexive,
+  assume h x k,
+  have nk := h k,
+  contradiction,
 end
 
 -- B
 example : irreflexive r → transitive r → asymmetric r :=
 begin
+  unfold irreflexive transitive,
+  assume h k,
+  assume x y,
+  assume rxy,
+  assume nryx,
+  have f := k rxy nryx,
+  have nrxx := h x,
+  contradiction,
 end
 
 -- C
